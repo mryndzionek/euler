b91a2255d53ee6e9d4395ea93f47589cde8e6371
Mariusz Ryndzionek <mryndzionek@gmail.com>
2018-05-30 19:29:56 +0200

    Moved all the "continued fractions" problems to a separate module

--- src/Solutions.hs
+++ src/Solutions.hs
@@ -27,6 +27,7 @@ import Euler.P54
 import Euler.P60
 import Euler.P61
 import Euler.P62
+import Euler.P64_66
 import Euler.Util
 
 p1 :: Solution
@@ -538,31 +539,6 @@ p59 input = show $ sum $ zipWith xor ciphertxt (cycle key)
 
 p63 :: Solution
 p63 _ = show . sum $ ([floor(1 / ((1 :: Double) - logBase 10 n)) | n <- [1..9]] :: [Integer])
-
-p64 :: Solution
-p64 input = show . length $ filter odd $ map (subtract 1 . length . compute) [2..limit]
-    where
-    limit = read input :: Integer
-    compute n = evalState (cont (n, 0, 1)) []
-    cont :: (Integer, Integer, Integer) -> State [Integer] [Integer]
-    cont (r, n, d)
-        | d == 0 = get
-        | d == 1 && n /= 0 = update >> get
-        | otherwise = update >> cont (r, -a, (r - a ^ (2 :: Integer)) `div` d)
-        where
-            m = (truncate (sqrt (fromIntegral r) :: Double) + n) `div` d
-            a = n - d * m
-            update = modify $ \s -> s ++ [m]
-
-p65 :: Solution
-p65 input = show.sum $ map digitToInt.show.numerator.fraction $ take limit e
-    where
-        limit = read input :: Int
-        e = [2, 1] ++ concat [[n, 1, 1] | n <- [2,4..] :: [Integer]]
-        fraction = foldr cont 0
-        cont x 0 = x % 1
-        cont x y = (x % 1) + (1 / y)
-
 
 solutions :: Map.Map Int (Solution, IO String)
 solutions = Map.fromList [
